5c9312c2822589d3409facd4dc13e05a69f007bb
DenniDiamond <bedarevvadik3331@gmail.com>
2022-03-15 12:00:52 +0200

    add more function

--- script/index.js
+++ script/index.js
@@ -5,6 +5,72 @@ const optionBtnPeriod = document.querySelector('.option__btn_period');
 const optionListOrder = document.querySelector('.option__list_order');
 const optionListPeriod = document.querySelector('.option__list_period');
 
+const decLoOfNum = (n, titles) =>  n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+    0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+
+const topCityBtn = document.querySelector('.top__city');
+const city = document.querySelector('.city');
+const cityClose = document.querySelector('.city__close');
+const cityRegionList = document.querySelector('.city__region-list');
+const cityList = document.querySelector('.city__list');  
+const overlayVacancy = document.querySelector('.overlay_vacancy');
+const resultList = document.querySelector('.result__list');
+const formSearch = document.querySelector('.bottom__search');
+const found = document.querySelector('.found');
+
+
+const getData = ({search, id} = {}) => {
+    if (search) {
+        return   fetch(`http://localhost:3000/api/vacancy?search=${search}`).then(response => response.json());
+    };
+    return   fetch(`http://localhost:3000/api/vacancy/${id ? id : ''}`).then(response => response.json());
+};
+
+const createCard = (vacancy) => {
+
+    const {title, id, compensation, workSchedule, employer, address, description, date} = vacancy;
+
+    const card = document.createElement('li');
+    card.classList.add('result__item');
+
+ 
+    card.insertAdjacentHTML('afterbegin', `
+          <article class="vacancy">
+          <h2 class="vacancy__title">
+              <a class="vacancy__open-modal" href="#" data-vacancy="${id}">${title}</a>
+          </h2>
+          <p class="vacancy__compensation">${compensation}</p>
+          <p class="vacancy__work-schedule">${workSchedule}</p>
+          <div class="vacancy__employer">
+              <p class="vacancy__employer-title">${employer}</p>
+              <p class="vacancy__employer-address">${address}</p>
+          </div>
+          <p class="vacancy__description">${description}</p>
+
+          <p class="vacancy__date">
+              <time datetime="2022-01-25">${date}</time>
+          </p>
+          <div class="vacancy__wrapper-btn">
+              <a class="vacancy__response vacancy__open-modal" href="#" data-vacancy="3515">Откликнуться</a>
+              <button class="vacancy__contacts">Показать контакты</button>
+          </div>
+          </article>
+
+    `);
+    return card;
+};
+
+const renderCards = (data) => {
+    resultList.textContent = '';
+    const cards = data.map(createCard);
+    resultList.append(...cards);
+};
+
+const init = async () => {
+    const data = await getData();
+    renderCards(data);
+};
+
 optionBtnOrder.addEventListener('click', () => {
     optionListOrder.classList.toggle('option__list_active');
     optionListPeriod.classList.remove('option__list_active');
@@ -49,15 +115,10 @@ optionListPeriod.addEventListener('click', (event) => {
         }
         target.classList.add('option__item_active');
     }
-})
+});
 
 // выбор города
 
-const topCityBtn = document.querySelector('.top__city');
-const city = document.querySelector('.city');
-const cityClose = document.querySelector('.city__close');
-const cityRegionList = document.querySelector('.city__region-list');
-const cityList = document.querySelector('.city__list');
 
 topCityBtn.addEventListener('click', () => {
     city.classList.toggle('city_active');
@@ -66,6 +127,8 @@ topCityBtn.addEventListener('click', () => {
 cityClose.addEventListener('click', () => {
     city.classList.remove('city_active');
 });
+
+
 
 cityRegionList.addEventListener('click', (event) => {
     const target = event.target;
@@ -79,74 +142,110 @@ cityRegionList.addEventListener('click', (event) => {
 });
 
 // модальное окно
+const createModal = (data) => {
+   const {address,
+    compensation,
+    description,
+    employer,
+    employment,
+    experience,
+    skills,
+    title
+} = data;
+const modal = document.createElement('div');
+modal.classList.add('modal');;
 
-const overlayVacancy = document.querySelector('.overlay_vacancy');
-const resultList = document.querySelector('.result__list');
+const closeButtonElem = document.createElement('button');
+closeButtonElem.classList.add('modal__close');
+closeButtonElem.textContent = 'X';
 
-resultList.addEventListener('click', (e) => {
+const titleElem = document.createElement('h2');
+titleElem.classList.add('modal__title');
+titleElem.textContent = title;
+
+const compensationElem = document.createElement('p');
+compensationElem.classList.add('modal__compensation');
+compensationElem.textContent = compensation;
+
+const employerElem = document.createElement('p');
+employerElem.classList.add('modal__employer');
+employerElem.textContent = employer;
+
+const addressElem = document.createElement('p');
+addressElem.classList.add('modal__address');
+addressElem.textContent = address;
+
+const experienceElem = document.createElement('p');
+experienceElem.classList.add('modal__experience');
+experienceElem.textContent = experience;
+
+const employmentElem = document.createElement('p');
+employmentElem.classList.add('modal__employment');
+employerElem.textContent = employment;
+
+const descriptionElem = document.createElement('p');
+ descriptionElem.classList.add('modal__description');
+description.textContent = description;
+
+const skillsElem = document.createElement('div');
+skillsElem.classList.add('modal__skills', 'skills');
+
+const skillsTitleElem = document.createElement('h3');
+skillsTitleElem.classList.add('skills__title');
+skillsTitleElem.textContent = 'Подробнее:';
+
+const skillsListElem = document.createElement('ul');
+skillsListElem.classList.add('skills__list');
+
+ for (const skill of skills) {
+    const skillsItemElem = document.querySelector('li');
+    skillsItemElem.classList.add('skills__item');
+    skillsElem.textContent = skill;
+    skillsListElem.append(skillsItemElem);
+ }
+
+ skillsElem.append(skillsTitleElem, skillsListElem);
+
+ const submitButtonElem = document.createElement('button');
+ submitButtonElem.classList.add('modal__response');
+ submitButtonElem.textContent = 'Отправить резюме';
+
+ modal.append(
+     closeButtonElem,
+     titleElem,
+     compensationElem,
+     employerElem,
+     addressElem,
+     experienceElem,
+     employerElem,
+     descriptionElem,
+     skillsElem,
+     submitButtonElem
+ );
+
+    return modal;
+};
+let modal = null;
+resultList.addEventListener('click', async (e) => {
     const target = e.target;
 
     if (target.dataset.vacancy) {
         e.preventDefault();
         overlayVacancy.classList.add('overlay_active');
+        const data = await getData({id: target.dataset.vacancy});
+        modal = createModal(data);
+        overlayVacancy.append(modal);
     }
-})
+});
 overlayVacancy.addEventListener('click', (e) => {
     const target = e.target;
     if (target === overlayVacancy || target.classList.contains('modal__close')) {
     overlayVacancy.classList.remove('overlay_active');
-}
-})
+    modal.remove();
+    }
+});
 
 ///вывод карточек
-
-const createCard = (vacancy) => {
-
-    const {title, id, compensation, workSchedule, employer, address, description, date} = vacancy;
-
-    const card = document.createElement('li');
-    card.classList.add('result__item')
-
-
-    card.insertAdjacentHTML('afterbegin', `
-          <article class="vacancy">
-          <h2 class="vacancy__title">
-              <a class="vacancy__open-modal" href="#" data-vacancy="${id}">${title}</a>
-          </h2>
-          <p class="vacancy__compensation">${compensation}</p>
-          <p class="vacancy__work-schedule">${workSchedule}</p>
-          <div class="vacancy__employer">
-              <p class="vacancy__employer-title">${employer}</p>
-              <p class="vacancy__employer-address">${address}</p>
-          </div>
-          <p class="vacancy__description">${description}</p>
-
-          <p class="vacancy__date">
-              <time datetime="2022-01-25">${date}</time>
-          </p>
-          <div class="vacancy__wrapper-btn">
-              <a class="vacancy__response vacancy__open-modal" href="#" data-vacancy="3515">Откликнуться</a>
-              <button class="vacancy__contacts">Показать контакты</button>
-          </div>
-          </article>
-
-    `);
-    return card;
-};
-
-const renderCards = (data) => {
-    resultList.textContent = '';
-    const cards = data.map(createCard);
-    resultList.append(...cards);
-};
-
-const getData = ({search} = {}) => {
-    if (search) {
-        return   fetch(`http://localhost:3000/api/vacancy?search=${search}`).then(response => response.json());
-    }
-    return   fetch('http://localhost:3000/api/vacancy').then(response => response.json());
-}
-const formSearch = document.querySelector('.bottom__search');
 
 formSearch.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -154,28 +253,18 @@ formSearch.addEventListener('submit', async (e) => {
 
 
     if(textSearch.length > 2) {
-        formSearch.search.style.borderColor = ''
+        formSearch.search.style.borderColor = '';
         const data = await getData({search: textSearch});
         renderCards(data);
+        found.innerHTML = `${decLoOfNum(data.length, ['вакансия', 'вакансии', 'вакансий'])} вакансий &laquo;${textSearch}&raquo;`;
         formSearch.reset();
-
-        const haad = document.createElement('h1');
-        haad.classList.add('found__item')
-        haad.insertAdjacentHTML('afterbegin', `
-        class="found">${data.length} вакансий &laquo;<span class="found__item">${textSearch}</span>&raquo;
-        `);
-        return haad;
     } else {
-        formSearch.search.style.borderColor = 'red'
+        formSearch.search.style.borderColor = 'red';
         setTimeout(() => {
             formSearch.search.style.borderColor = '';
-        }, 3000);
+        }, 2000);
     }
-})
+});
 
-const init = async () => {
-    const data = await getData();
-    renderCards(data);
-};
 
 init();
